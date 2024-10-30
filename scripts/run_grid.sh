823fd6f005e7026c63049aede7a780e9e03acc0d
Francesco Mannella <francesco.mannella@gmail.com>
2024-10-30 18:12:17 +0100

    run_grid.sh

--- scripts/run_grid.sh
+++ scripts/run_grid.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 seeds=1
-decay_speeds='5 4'
-local_decay_speeds=$(seq 2.0 -0.5 0.5)
+decay_speeds='4'
+local_decay_speeds='1.5'
+wandb=false
+# decay_speeds='5 4'
+# local_decay_speeds=$(seq 2.0 -0.5 0.5)
 CURR_DIR=$(pwd)
 EXE=$(dirname "$0" | xargs realpath | sed -e "s/scripts/src\/main.py/" ) 
 
@@ -13,11 +16,12 @@ for s in $seeds; do
                 printf "s_%04d_ds_%08d_lds_%08d" \
                     "$s" \
                     "$(printf "%.0f" $(echo "$ds * 1000" | bc))" \
-                    "$(printf "%.0f" $(echo "$lds * 1000" | bc))"
+                    "$(printf "%10.0f" $(echo "$lds * 1000" | bc))"
             )
 
             mkdir -p $dirname
             cd $dirname
+            if [[ $wandb == false ]]; then wandb disabled; fi
             python $EXE --seed $s --decaying_speed $ds --local_decaying_speed $lds
             cd $CURR_DIR
         done
